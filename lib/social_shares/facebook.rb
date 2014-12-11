2e3ca6ad7e1db64aae726df1e55aed2c3f629701
Hamed Ramezanian <iceage2098@gmail.com>
2014-12-11 15:57:58 +0000

    Use fql syntax for getting facebook shares count

--- lib/social_shares/facebook.rb
+++ lib/social_shares/facebook.rb
@@ -1,17 +1,23 @@
 module SocialShares
   class Facebook < Base
-    URL = 'http://api.ak.facebook.com/restserver.php'
+    URL = 'https://graph.facebook.com/fql'
 
     def shares!
       response = RestClient.get(URL, {
         :params => {
-          :urls => checked_url,
-          :v => '1.0',
-          :method => 'links.getStats',
-          :format => 'json'
+          :q => "SELECT url, normalized_url, share_count,
+            like_count, comment_count, total_count,commentsbox_count,
+            comments_fbid, click_count
+            FROM link_stat WHERE url='#{fql_escape(checked_url)}'"
         }
       })
-      JSON.parse(response)[0]['share_count'] || 0
+      JSON.parse(response)['data'][0]['share_count'] || 0
+    end
+
+    private
+
+    def fql_escape(str)
+      str.gsub("'", "\\\\'")
     end
   end
 end
